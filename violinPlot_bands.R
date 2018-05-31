3cf386efc76f43a65360f70f10707ab3615f9455
Ryan Brady <rpbrady@ucdavis.edu>
2018-05-31 14:16:15 -0700

    Fix plot legends and put commands in order

--- violinPlot_bands.R
+++ violinPlot_bands.R
@@ -1,30 +1,28 @@
 #Density plots / violin plots for the most commonly used crops all color bands
 
 #violin plot for each color band , mean median sd
-cropColor <- read_csv("~/github/GeoSpatial-Classification/updateddf.csv")
-
+cropColor <- read.csv("updateddf.csv")
+x = split.data.frame(cropColor,cropColor$crop_list)
+nuts = rbind.data.frame(x$ALMOND,x$WALNUT)
+getwd()
 library(stringi)
 library(ggplot2)
 library(grid)
 library(gridExtra)
-install.packages("gridExtra")
+#install.packages("gridExtra")
 
 
 colnames(nuts(7)) = c('band1sd')
-names(nuts)
 names(nuts)[names(nuts) == 'Band1std deviation'] = 'band1sd'
-View(nuts)
 
-x = split.data.frame(cropColor,cropColor$crop_list)
-nuts = rbind.data.frame(x$ALMOND,x$WALNUT)
-as.factor(nuts$crop_list)
+
 nutMean1 <- ggplot(nuts, aes(factor(nuts$crop_list), Band1mean))
-nutMean1 + geom_violin(aes(fill = factor(nuts$crop_list)),draw_quantiles = c(0.25, 0.5, 0.75)) + geom_jitter(height = 0, width = 0.1) +
+nutMean1 + geom_violin(aes(fill = factor(nuts$crop_list)),draw_quantiles = c(0.25, 0.5, 0.75)) + guides(fill=guide_legend(title='Type of Nut'))+
   xlab('Crop') + ylab('Mean') + ggtitle('Violin Plot of Band 1 Means of Commonly Grown Nuts in Yolo County')
 
 nutMean2 <- ggplot(nuts, aes(factor(nuts$crop_list), Band2mean))
-nutMean2 + geom_violin(aes(fill = factor(nuts$crop_list)),draw_quantiles = c(0.25, 0.5, 0.75)) + geom_jitter(height = 0, width = 0.1) +
-   xlab('Crop') + ylab('Mean') + ggtitle('Violin Plot of Band 2 Means of Commonly Grown Nuts in Yolo County')
+nutMean2 + geom_violin(aes(fill = factor(nuts$crop_list)),draw_quantiles = c(0.25, 0.5, 0.75))  +
+   xlab('Crop') + ylab('Mean') + ggtitle('Violin Plot of Band 2 Means of Commonly Grown Nuts in Yolo County') + guides(fill=guide_legend(title='Type of Nut'))
 
 nutMean3 <- ggplot(nuts, aes(factor(nuts$crop_list), Band3mean))
 nutMean3 + geom_violin(aes(fill = factor(nuts$crop_list)),draw_quantiles = c(0.25, 0.5, 0.75)) + geom_jitter(height = 0, width = 0.1) +
