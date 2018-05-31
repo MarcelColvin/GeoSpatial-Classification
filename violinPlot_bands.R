9c0507c888932e367ad9efda87e8f659019b811e
snadel621 <35207048+snadel621@users.noreply.github.com>
2018-05-31 11:19:44 -0700

    Add files via upload

--- violinPlot_bands.R
+++ violinPlot_bands.R
@@ -1,53 +1,14 @@
 #Density plots / violin plots for the most commonly used crops all color bands
 
 #violin plot for each color band , mean median sd
-
-
-
 cropColor <- read_csv("~/github/GeoSpatial-Classification/updateddf.csv")
-sort(table(updateddf$crop_list))
-tapply(cropColor$Band1mean,cropColor$crop_list, mean)
-grepl(cropColor$crop_list[5],'UNCULTIVATED')
-install.packages('stringi')
-library(stringi)
-
-fallows = sapply(X = cropColor$crop_list,FUN = function(x){
-  bool = stri_detect_fixed(x,'UNCULTIVATED')
-  return(bool)
-})
-View(fallows)
-sum(fallows)
-fallowcrops = cropColor[fallows,]
-nonfallow = cropColor[!fallows,]
-View(nonfallow)
-activecrops = nonfallow[nonfallow$is_active == 1,]
-dim(activecrops)
-activecrops$crop_list
-nonfallow[nonfallow$is_active == 1,]
-listings =tapply(activecrops$crop_list,activecrops$crop_list,length)
 
 library(stringi)
-
-
-cropColor[fallows,]
 library(ggplot2)
-sum(fallows)
-p8 <- ggplot(fallowcrops, aes(x=Band8third,color = as.factor(fallowcrops$is_active))) + 
-  geom_density()
 library(grid)
 library(gridExtra)
 install.packages("gridExtra")
-grid.arrange(p8,p9,p10, ncol = 1,nrow = 3, top = "Main title")
-p1
-p2
-cropColor$Band1first
-par(mfrow = c(3, 2)) 
-sort(table(nonfallow$crop_list))
 
-table(fallowcrops$is_active)
-plot1 <- qplot(1)
-plot2 <- qplot(1)
-grid.arrange(plot1, plot2, ncol=2)
 
 colnames(nuts(7)) = c('band1sd')
 names(nuts)
