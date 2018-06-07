5b9dae94837afed8f1793d96ceb8080a2b332b18
Ryan Brady <rpbrady@ucdavis.edu>
2018-06-06 22:28:12 -0700

    OVERWRITE``

--- YoloCountyChristmasTreePlot.R
+++ YoloCountyChristmasTreePlot.R
@@ -1,10 +1,10 @@
-
+###Plot Crop Correct vs Incorrect
 library(maptools)
 library(rgdal)
 
 library(raster)
 
-dfhold = read.csv('Desktop/STA160/GeoSpatial-Classification/classless.csv')
+dfhold = read.csv('Desktop/STA160/GeoSpatial-Classification/Data/classless.csv')
 
 Cropshape = readOGR('Desktop/STA160/GeoSpatial-Classification/Crops_2016/Crops_2016_26910.shp')
 
@@ -18,3 +18,30 @@ plot(classified_df,col = as.character(hopeandpray$correct),main = 'Correct Vs In
 
 legend(557143.9, 4256037, legend=c("Correct", "Incorrect"),
        col=c("green", "red"), lty=1, cex=0.8)
+
+#####Plot Actual Crops vs Predicted
+
+cropdata = read.csv('Desktop/STA160/GeoSpatial-Classification/Data/predcitspluscrops.csv')
+cropdata
+cropdata$crcop
+
+cropinfo= merge(Cropshape,cropdata[,c('OBJECTID','correct','predictions')],by ='OBJECTID') 
+realcrops = cropinfo[!is.na(cropinfo$correct),]
+realcrops$predictions
+plot(realcrops,col = realcrops$correct,main = 'Actual Crops in Yolo County')
+legend(555143.9, 4283037,c('OAT','ALFALFA','SAFFLOWER',
+                                  'WALNUT','TOMATO','ALMOND','WHEAT','SUNFLOWER','GRAPE','RICE',
+                                  'CORN'),col=1:length(realcrops$correct),pch=8)
+
+plot(realcrops,col = realcrops$predictions,main = 'Predicted Crops in Yolo County')
+legend(555143.9, 4283037,c('OAT','ALFALFA','SAFFLOWER',
+                           'WALNUT','TOMATO','ALMOND','WHEAT','SUNFLOWER','GRAPE','RICE',
+                           'CORN'),col=1:length(realcrops$predictions),pch=8)
+
+
+
+
+
+
+
+       
